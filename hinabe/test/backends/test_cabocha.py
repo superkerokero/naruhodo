e4f733f3b473538123030b0451470f5e65181267
superkerokero <superkerokero@outlook.com>
2017-12-14 16:47:08 +0900

    Fixed test on cabocha.py.

--- hinabe/test/backends/test_cabocha.py
+++ hinabe/test/backends/test_cabocha.py
@@ -6,9 +6,9 @@ class TestCabochaClient(unittest.TestCase):
         cabo = CabochaClient()
         text = '* 0 2D 1/2 0.698846\n麻生\t名詞,固有名詞,人名,姓,*,*,麻生,アソウ,アソー\n太郎\t名詞,固有名詞,人名,名,*,*,太郎,タロウ,タロー\nは\t助詞,係助詞,*,*,*,*,は,ハ,ワ\n* 1 2D 0/1 3.258964\nコーヒー\t名詞,一般,*,*,*,*,コーヒー,コーヒー,コーヒー\nを\t助詞,格助詞,一般,*,*,*,を,ヲ,ヲ\n* 2 7D 0/2 1.034467\n飲み\t動詞,自立,*,*,五段・マ行,連用形,飲む,ノミ,ノミ\nませ\t助動詞,*,*,*,特殊・マス,未然形,ます,マセ,マセ\nん\t助動詞,*,*,*,不変化型,基本形,ん,ン,ン\n。\t記号,句点,*,*,*,*,。,。,。\n'
         cabo.add(text)
-        self.assertEqual(cabo.chunks[0].main, "麻生太郎")
+        self.assertEqual(cabo.chunks[0].main, "麻生\n太郎")
         self.assertEqual(cabo.chunks[1].main, "コーヒー")
-        self.assertEqual(cabo.chunks[2].main, "飲む(否定)")
+        self.assertEqual(cabo.chunks[2].main, "飲む\n(否定)")
         self.assertEqual(cabo.chunks[0].func, "は")
         self.assertEqual(cabo.chunks[1].func, "を")
         self.assertEqual(cabo.chunks[2].func, "ませ・ん。")
