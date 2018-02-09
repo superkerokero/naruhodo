1c233d62c79f99b395ece937e200fee922e974e9
superkerokero_sugamo2 <superkerokero@outlook.com>
2018-02-09 21:47:47 +0900

    Fixed setup.py for name change.

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@
 from distutils.core import setup
 
 setup(
-    name = 'hinabe',
+    name = 'naruhodo',
     packages = ['src'],
     version = '0.1',
     description = 'A toolkit providing basic functionalities of Japanese NLP.',
     author = 'superkerokero',
     author_email = 'superkerokero@outlook.com',
-    url = 'https://github.com/superkerokero/hinabe',
+    url = 'https://github.com/superkerokero/naruhodo',
     download_url = 'download link you saved',
     keywords = ['nlp', 'japanese'],
     classifiers = [],
