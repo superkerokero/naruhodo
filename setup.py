dbb8511737dd407cc0d6f93b45cd04876f40ccea
superkerokero <superkerokero@outlook.com>
2018-02-14 21:13:37 +0900

    Updated version to 0.1.4.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name = 'naruhodo',
     packages = find_packages(exclude=['contrib', 'docs', 'test']),
-    version = '0.1.3',
+    version = '0.1.4',
     description = 'A python library for generating dependency graph(DG) and knowledge graph(KG) from Japanese text.',
     long_description = """
     Naruhodo is a python library for generating dependency-graph(DG) and knowledge-graph(KG) in networkx format from Japanese text or urls that contains Japanese texts. You can visualize these graphs directly in jupyter notebook or export graphs to JSON file for visualization using external programs.
